42073ca7bd953d0722272ec5b3338e2bf4a37342
ankit_049 <ankitrajputji00@gmail.com>
2024-05-14 01:42:30 +0530

    Build and Deploy for Testing

--- routes/Brands.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import  {createBrand , fetchBrands} from '../controller/Brands.controller.js'  
-
-const router = express.Router(); 
-
-router.get('/' , fetchBrands).post('/' ,createBrand); 
-
-
-export default router;
--- routes/Category.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import  {fetchCategories , createCategory} from '../controller/Category.controller.js'  
-
-const router = express.Router(); 
-
-router.get('/' , fetchCategories).post('/',createCategory); 
-
-
-export default router;
